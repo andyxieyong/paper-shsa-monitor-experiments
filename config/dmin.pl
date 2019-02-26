902812eae8235e75b04639ce9fddf3528ecc806c
Denise Ratasich <dratasich@users.noreply.github.com>
2019-02-26 16:21:56 +0100

    add launch file for monitor

---
+++ config/dmin.pl
@@ -0,0 +1,50 @@
+%
+% SHSA Knowledge Base for Daisy's dmin Calculation
+%
+% Denise Ratasich
+% 2019-02-26
+%
+
+:- use_module(library(shsa)).
+
+
+% SHSA knowledge base from
+% Ratasich et al.: Fault detection. 2019.
+%
+
+% dmin
+function(dmin, r1, d_2d).
+function(d_2d, r2, d_3d).
+function(d_2d, r3, [map, pose]).
+function(dmin, r4, [dmin_last, speed]).
+function(dmin_last, r5, [dmin]).
+% ROS msg types of distance data different -> separate variables
+%% function(d_2d, rlaser, laserscan)
+%% function(d_2d, rsonar, sonararray)
+%% function(d_3d, rpointcloud2, pointcloud2)
+
+% to create executable substitutions: define implementations of the relations
+implementation(r1, "dmin.v = min(d_2d.v)").
+implementation(r2, "
+height = int(len(d_3d.v)/2)
+d_2d.v = list(d_3d.v[height])
+").
+implementation(r3, "
+# https://answers.ros.org/question/227400/nearest-obstacle-in-map/
+# http://docs.ros.org/api/nav_msgs/html/msg/OccupancyGrid.html
+# TODO
+").
+implementation(r4, "").
+implementation(r5, "").
+% helpers: convert ROS msg types
+%% implementation(rlaser, "d_2d.v = list(laserscan.v.ranges)").
+%% implementation(rsonar, "d_2d.v = list(sonararray.v.ranges)").
+%% implementation(rsonar, "d_3d.v = list(image.v.data)").
+
+% hard-code provided itoms
+% (though the itoms can be extracted by calling `rostopic list`,
+% a mapping to the variables is needed, so we just statically define it here)
+itomsOf(dmin, ["/emergency_stop/dmin/data"]).
+itomsOf(d_2d, ["/scan/ranges", "/p2os/sonar/ranges"]).
+%% itomsOf(pointcloud2, ["/tof/points"]).
+%% itomsOf(pose, ["/amcl_pose"]).
